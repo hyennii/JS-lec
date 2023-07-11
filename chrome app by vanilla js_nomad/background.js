bce4656a3fcf0a8f164f67701c5fb5af4ea70969
Jeon Hyewon <107667966+hyennii@users.noreply.github.com>
2023-07-11 17:44:45 +0900

    Delete background.js

--- chrome app by vanilla js_nomad/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];     //img 폴더 속 이미지들과 이름 같게
-
-const chosenImage = images[Math.floor(Math.random() * images.length)]
-
-const bgImage = document.createElement("img");      //js에서 html element 생성
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);   //body에 html 추가
